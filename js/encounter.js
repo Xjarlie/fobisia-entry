c24caa998582fb2b20e2a02b2302f7b37f679f00
Charlie Appleton <xjarlie@gmail.com>
2021-02-20 10:51:36 +0900

    changes

--- js/encounter.js
+++ js/encounter.js
@@ -6,12 +6,14 @@ class Encounter {
     }
 
     begin() {
+        terminal.denyInput();
+
         terminal.print('\n-------------------');
         terminal.print('An Encounter has begun!');
         terminal.print('You are now in combat with ' + this.enemy.name + '.');
         
         while (this.player.health != 0 && this.enemy.health != 0 && this.player.stamina != 0) {
-            terminal.print()
+            terminal.print('It is your turn - type \'attack\', \'defend\' or \'spell\'');
         }
     }
 
